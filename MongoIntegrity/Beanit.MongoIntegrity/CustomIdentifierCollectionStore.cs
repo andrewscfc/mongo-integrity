877d2ca8a547293312735429b9b750301630604e
Andrew Bennett <andrew_scfc@yahoo.co.uk>
2014-11-14 22:20:29 +0000

    Started defining store structure

---
+++ MongoIntegrity/Beanit.MongoIntegrity/CustomIdentifierCollectionStore.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Beanit.MongoIntegrity
+{
+    public abstract class CustomIdentifierCollectionStore<TIdentifier, TDocument> : ICollectionStore<TIdentifier, TDocument> where TDocument : IDocument<TIdentifier>
+    {
+        public abstract string CollectionName { get; }
+
+        public IEnumerable<TDocument> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TDocument Get(TIdentifier id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TDocument document)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(TDocument document)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(TIdentifier id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MongoIntegrity/Beanit.MongoIntegrity/ICollectionStore.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beanit.MongoIntegrity
+{
+    public interface ICollectionStore<in TIdentifier, TDocument> where TDocument : IDocument<TIdentifier>
+    {
+        IEnumerable<TDocument> Get();
+
+        TDocument Get(TIdentifier id);
+
+        void Update(TDocument document);
+
+        void Add(TDocument document);
+
+        void Delete(TIdentifier id);
+
+    }
+}
